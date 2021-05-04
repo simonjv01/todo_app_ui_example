432e42fe537ade516c00e34336414efc33d23a85
Simon Vargas <sjvargas01@gmail.com>
2021-05-04 13:07:59 -0400

    implemented edit on click of todo item

--- lib/widget/todo_widget.dart
+++ lib/widget/todo_widget.dart
@@ -41,51 +41,54 @@ class TodoWidget extends StatelessWidget {
         ),
       );
 
-  Widget buildTodo(BuildContext context) => Container(
-        color: Colors.white,
-        padding: EdgeInsets.all(20),
-        child: Row(
-          children: [
-            Checkbox(
-              activeColor: Theme.of(context).primaryColor,
-              checkColor: Colors.white,
-              value: todo.isDone,
-              onChanged: (_) {
-                final provider =
-                    Provider.of<TodosProvider>(context, listen: false);
-                final isDone = provider.toggleTodoStatus(todo);
+  Widget buildTodo(BuildContext context) => GestureDetector(
+        onTap: () => editTodo(context, todo),
+        child: Container(
+          color: Colors.white,
+          padding: EdgeInsets.all(20),
+          child: Row(
+            children: [
+              Checkbox(
+                activeColor: Theme.of(context).primaryColor,
+                checkColor: Colors.white,
+                value: todo.isDone,
+                onChanged: (_) {
+                  final provider =
+                      Provider.of<TodosProvider>(context, listen: false);
+                  final isDone = provider.toggleTodoStatus(todo);
 
-                Utils.showSnackBar(
-                  context,
-                  isDone ? 'Task Completed' : 'Task marked incomplete',
-                );
-              },
-            ),
-            const SizedBox(width: 20),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    todo.title,
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Theme.of(context).primaryColor,
-                      fontSize: 22,
-                    ),
-                  ),
-                  if (todo.description.isNotEmpty)
-                    Container(
-                      margin: EdgeInsets.only(top: 4),
-                      child: Text(
-                        todo.description,
-                        style: TextStyle(fontSize: 20, height: 1.5),
-                      ),
-                    )
-                ],
+                  Utils.showSnackBar(
+                    context,
+                    isDone ? 'Task Completed' : 'Task marked incomplete',
+                  );
+                },
               ),
-            ),
-          ],
+              const SizedBox(width: 20),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      todo.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 22,
+                      ),
+                    ),
+                    if (todo.description.isNotEmpty)
+                      Container(
+                        margin: EdgeInsets.only(top: 4),
+                        child: Text(
+                          todo.description,
+                          style: TextStyle(fontSize: 20, height: 1.5),
+                        ),
+                      )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       );
 
